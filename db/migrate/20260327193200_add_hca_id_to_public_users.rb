1e625a8cb711dee429cc2f2d25d9ebf59e90328b
End <contact@endoftimee.tech>
2026-04-01 10:02:28 -0700

    work?

--- db/migrate/20260327193200_add_hca_id_to_public_users.rb
+++
@@ -1,6 +0,0 @@
-class AddHcaIdToPublicUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :public_users, :hca_id, :string unless column_exists?(:public_users, :hca_id)
-    add_index :public_users, :hca_id, unique: true unless index_exists?(:public_users, :hca_id)
-  end
-end
